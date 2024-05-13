850ca7d5e261c797547c158d3e27725430f75821
lamxuanbao <lamxuanbao.tb@gmail.com>
2024-05-13 14:56:55 +0700

    update

--- resources/views/pages/vector_store/create.blade.php
+++ resources/views/pages/vector_store/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
     <div class="container p-5">
-        <form enctype="multipart/form-data" method="post" action="{{url('admin/vector-store')}}">
+        <form enctype="multipart/form-data" method="post" action="{{url('vector-store')}}">
             @csrf
             @include('pages.vector_store.includes.form')
         </form>
