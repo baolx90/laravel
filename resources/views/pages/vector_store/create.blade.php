9ef41e6f4bd2a932eafdf270d13ebec2b6f497d5
lamxuanbao <lamxuanbao.tb@gmail.com>
2024-05-13 14:55:23 +0700

    update

--- resources/views/pages/vector_store/create.blade.php
+++ resources/views/pages/vector_store/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.default')
 @section('content')
     <div class="container p-5">
-        <img src="http://localhost/public/storage/vector-stores/bAIfB4Dqy9.1715583637.png"/>
         <form enctype="multipart/form-data" method="post" action="{{url('admin/vector-store')}}">
             @csrf
             @include('pages.vector_store.includes.form')
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatBotController;
 use App\Http\Controllers\VectorStoreController;
 
-Route::controller(ChatBotController::class)->group(function () {
-    Route::get('', 'index');
-    Route::get('bot/{id}', 'show');
-});
-
-Route::prefix('admin')->group(function () {
-   Route::resource('vector-store',VectorStoreController::class);
-});
+//Route::controller(ChatBotController::class)->group(function () {
+//    Route::get('', 'index');
+//    Route::get('bot/{id}', 'show');
+//});
+//
+//Route::prefix('admin')->group(function () {
+//   Route::resource('vector-store',VectorStoreController::class);
+//});
+Route::resource('',VectorStoreController::class);
 
 //  Route::get('/{any}', function () {
 //      return view('app');
