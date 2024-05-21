aa63c5775456a113d79538581b8f540e57a85044
lamxuanbao <lamxuanbao.tb@gmail.com>
2024-05-21 10:45:24 +0700

    .

--- resources/views/pages/chatbot.blade.php
+++ resources/views/pages/chatbot.blade.php
@@ -402,13 +402,13 @@
             @if($conversation['role'] == 'user')
                 <div class="message-role user">User</div>
                 <div class="user-message">
-                    {!! str_replace('\n','<br/>',$conversation['content']) !!}
+                    {!! nl2br($conversation['content']) !!}
                 </div>
             @endif
             @if($conversation['role'] == 'assistant')
                 <div class="message-role">Assistant</div>
                 <div class="assistant-message">
-                    {!! str_replace('\n','<br/>',$conversation['content']) !!}
+                    {!! nl2br($conversation['content']) !!}
                 </div>
             @endif
         @endforeach
